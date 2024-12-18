88612d22591ac1803e9a85365003335ced810e82
msotnikov <mishichka@gmail.com>
2024-12-18 12:37:21 +0300

    fix github actions

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true // mandatory, otherwise won't export
-  }
+  },
   // Optional: Change the output directory `out` -> `dist`
   // distDir: "build"
 }
